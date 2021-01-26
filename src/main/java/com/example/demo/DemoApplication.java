c46b2c25efb7757efe3f0e52a841bc1d5b60560a
stack-segment <com.sultanov.roman@gmail.com>
2021-01-26 10:37:18 +0500

    5k operations generator

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,41 +6,53 @@ import com.sun.istack.NotNull;
 import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.security.access.annotation.Secured;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
+@EnableCaching
 @SpringBootApplication
 public class DemoApplication {
+
+    public static int getRandIntFromInterval(int lb, int rb){
+        return (int)(lb + Math.random()*(rb - lb));
+    }
+
+    public static Operation operationGenerator(){
+        String[] currencies = new String[]{"AUD", "UZS", "EUR", "ALL",
+        "DZD", "XCD", "AOA","ARS","AMD","AWG","AFN","BSD","BDT","BBD",
+                "BHD","BZD","XOF","BGN","BOB","BIF","BTN","VES", "GHS"};
+
+        Operation operation = new Operation(
+                getRandIntFromInterval(0, 10),
+                getRandIntFromInterval(8600, Integer.MAX_VALUE),
+                getRandIntFromInterval(8600, Integer.MAX_VALUE),
+                new BigDecimal(getRandIntFromInterval(8600, Integer.MAX_VALUE)),
+                new BigDecimal(getRandIntFromInterval(8600, Integer.MAX_VALUE)),
+                currencies[getRandIntFromInterval(0, currencies.length)]
+                );
+        operation.setPaymentCommission(operation.getPaymentCommission().divide(new BigDecimal(100)));
+
+        return operation;
+    }
+
+    public static void generate5kOperations(OperationRepository repository) {
+        List<Operation> operationList = new ArrayList<>();
+        for (int i = 0; i < 5000; i++)
+            operationList.add(operationGenerator());
+        operationList.forEach(repository::save);
+    }
 
     public static void main(String[] args) {
         //SpringApplication.run(DemoApplication.class, args);
         ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class);
         OperationRepository repository = context.getBean(OperationRepository.class);
 
-//        Operation operation = repository.findById(5L).orElse(new Operation(1,
-//                        8600_1306,
-//                        8600_1204,
-//                        new BigDecimal(3_042_000),
-//                        new BigDecimal(6_420), "UZS"));
-//
-//
-//        System.out.println(operation.toString().hashCode());
-//        System.out.println(operation.toString());
-
-        List<Operation> operationList = List.of(new Operation(0, 8600_1309, 8600_1308, new BigDecimal(1_000_000), new BigDecimal(5_000), "USD"),
-                new Operation(0, 8600_1309, 8600_1201, new BigDecimal(12_042_000), new BigDecimal(75_420), "UZS"),
-                new Operation(1, 8600_1307, 8600_1202, new BigDecimal(1_042_000), new BigDecimal(8_420), "USD"),
-                new Operation(0, 8600_1309, 8600_1203, new BigDecimal(2_042_000), new BigDecimal(7_420), "USD"),
-                new Operation(1, 8600_1306, 8600_1204, new BigDecimal(3_042_000), new BigDecimal(6_420), "UZS"),
-                new Operation(1, 8600_1305, 8600_1205, new BigDecimal(5_042_000), new BigDecimal(5_420), "UZS"),
-                new Operation(0, 8600_1304, 8600_1206, new BigDecimal(7_042_000), new BigDecimal(3_420), "RUB"),
-                new Operation(1, 8600_1303, 8600_1207, new BigDecimal(9_042_000), new BigDecimal(1_420), "RUB"));
-
-        operationList.forEach(repository::save);
-
+        //generate5kOperations(repository);
 
     }
 
--- src/main/java/com/example/demo/service/OperationService.java
+++ src/main/java/com/example/demo/service/OperationService.java
@@ -4,6 +4,8 @@ package com.example.demo.service;
 import com.example.demo.model.Operation;
 import com.example.demo.repository.OperationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,13 +20,17 @@ public class OperationService {
         this.operationRepository = operationRepository;
     }
 
+
+
     public Operation getById(Long id){
         return this.operationRepository.findById(id).orElse(null);
     }
 
+
     public void saveInstance(Operation operation){
         this.operationRepository.save(operation);
     }
+
 
     public void deleteById(Long id){
         Operation operation = this.getById(id);
@@ -33,6 +39,7 @@ public class OperationService {
         }
     }
 
+    @Cacheable("id")
     public List<Operation> getAll(){
         return (List<Operation>)operationRepository.findAll();
     }
